4934d250a7aaa20f57f0d6b0e58f6a8a4aa48403
Fatima Ezzahra <felemenoun@gmail.com>
2022-10-29 21:51:29 +0100

    Create th sign in form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [data, setData] = useState({ username: '', password: '' });
+  const handleChanges = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+    console.log(data);
+  };
+
   return (
-    <div> hello </div>
+    <section>
+      <h1>Welcome back</h1>
+      <p>Welcome back Please enter your details</p>
+      <form>
+        <div>
+          <label htmlFor="username">username</label>
+          <input 
+          onChange={handleChanges}
+          type="text" id="username" name="username" value={data.username} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input 
+          onChange={handleChanges}
+          type="password" id="password" name="password" value={data.password} />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+    </section>
   )
 }
