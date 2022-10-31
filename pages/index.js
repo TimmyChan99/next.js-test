d26f6a168e41717a30b308e01692b5287c22ea04
Fatima Ezzahra <felemenoun@gmail.com>
2022-10-31 15:56:39 +0100

    Reedirect user after loging in

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { useRouter } from 'next/router'
 
 export default function Home() {
   const [data, setData] = useState({ username: '', password: '' });
   const [message, setMessage] = useState('');
+  const router = useRouter()
 
   const handleChanges = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
@@ -18,7 +20,10 @@ export default function Home() {
       body: JSON.stringify(data),
     });
     const json = await res.json();
-    if (json.status === 'success') localStorage.setItem('user', JSON.stringify({ user: data.username, status: true}));
+    if (json.status === 'success') {
+      localStorage.setItem('user', JSON.stringify({ user: data.username, status: true}));
+      router.push('/images');
+    }
     if (!res.ok) setMessage(json.message);
   };
 
