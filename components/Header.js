f620efa8a78b0767a69fbb137e1dd329bc864616
Fatima Ezzahra <felemenoun@gmail.com>
2022-11-01 13:54:22 +0100

    Update header style in images page

--- components/Header.js
+++ components/Header.js
@@ -4,20 +4,23 @@ const Header = ({ totalPages, pageNumber, prevPage, nextPage }) => {
 	const { user } = JSON.parse(localStorage.getItem('user')) || {}
 
 	return (
-		<header className="text-zinc-900 flex flex-col items-center justify-between px-5 py-3 space-y-4">
-			<h1 className="text-zinc-600 text-xl">Welcome back, <span className="font-bold">{user}</span></h1>
-			<div className="flex items-center justify-center space-x-5 w-full font-medium">
+		<header className="text-zinc-900 flex flex-col md:flex-row items-center justify-between px-5 py-3 space-y-4">
+			<h1 className="text-zinc-600 text-xl md:w-2/4 w-full text-center">Welcome back, <span className="font-bold">{user}</span></h1>
+			<div className="flex items-center justify-center space-x-5 w-full font-medium md:w-2/4">
 				{(pageNumber > 1) && (<button
-					className="bg-zinc-900 text-white px-5 py-2 rounded-md"
+					className="bg-zinc-200 text-zinc-900 px-5 py-2 rounded-md"
 					onClick={prevPage}>
 					Prev
 				</button>)}
 				{totalPages > 0 && <p
 					className="text-zinc-600"
-				>Page {pageNumber} of {totalPages}
+				> Page 
+				<span className="underline font-bold text-zinc-700 p-1"> {pageNumber} </span> 
+				of 
+				<span className="underline font-bold text-zinc-700 p-1"> {totalPages} </span>
 				</p>}
 				{(pageNumber <= totalPages) && (<button
-					className="bg-zinc-900 text-white px-5 py-2 rounded-md"
+					className="bg-zinc-200 text-zinc-900 px-5 py-2 rounded-md"
 					onClick={nextPage}>
 					Next
 				</button>)}
